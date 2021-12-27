2df352da5247e1121f96fe202203786dc4e4f0ca
adborbas <adam.borbas@tumblr.com>
2021-12-27 15:59:33 +0100

    ezaz

--- Sources/qBittorrent/qBittorrentWebAPI/Authentication/BasicAuthCredentials.swift
+++ Sources/qBittorrent/qBittorrentWebAPI/Authentication/BasicAuthCredentials.swift
@@ -11,4 +11,9 @@ import Alamofire
 public struct BasicAuthCredentials: Equatable {
     let username: String
     let password: String
+    
+    public init(username: String, password: String) {
+        self.username = username
+        self.password = password
+    }
 }
